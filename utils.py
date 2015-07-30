cb88a9800b2e403998ec6c99ba47bba91ab26c35
Jakob Bassinger <jemoryb@gmail.com>
2015-07-30 17:09:25 -0400

    save

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@
 import colors as c
 
 def ask(question):
-    print(c.blue + "what is your name" + c.reset)
+    print(c.blue + question + c.reset)
     answer = input("< " + c.base3).lower().strip()
     print(c.reset)
     return answer
